0f7d876ea2b7f4b26f9a467375812ebe476835a8
devCaiodias <caiodiasmartins12@gmail.com>
2025-08-23 12:22:10 -0300

    add app folder with layout.tsx and page.tsx file

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Arcadium 🚀</h1>
+      <p>Plataforma para reviews e rankings de jogos</p>
+    </main>
+  );
+}
